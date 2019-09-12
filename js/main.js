67bbf844c825c4f168df4373599e8fd01d8d8cc9
Chris Pizzurro <chris.pizzurro@slalom.com>
2019-09-12 11:54:42 -0400

    clean up the js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,53 @@
 ; var app = new (function($){
-  var self = this;
-  self.wamFileService = createFileService("http://localhost:62435");
-  self.utils = {
-    createUuid: create_UUID
-  };
-  return self;
-
-  function create_UUID() {
-    var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = (dt + Math.random() * 16) % 16 | 0;
-      dt = Math.floor(dt / 16);
-      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
-    return uuid;
+  function createUtils(){
+    /**
+     * Not necessary for the solution but there must be a wam domain level unique id that
+     * can be tied to the files
+     * @returns {string} Unique identifier
+     */
+    function create_UUID() {
+      var dt = new Date().getTime();
+      var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+      uuid.replace(/[xy]/g, function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        var hex = (c == "x" ? r : (r & 0x3 | 0x8));
+        return hex.toString(16);
+      });
+      return uuid;
+    }
+    var utils = {
+      createUuid: create_UUID
+    };
+    return utils;
   }
 
+  /**
+   * File service supports file uploads via the WAM APIs via jQuery
+   * and azure blob service via the azure-storage-blob.js client library
+   * @param {string} baseUri The URI for the WAM API
+   * @returns {{listFilesInContainer: listFilesInContainer, uploadFile: uploadFile, getTokenizedFileLink: getTokenizedFileLink}}
+   * Object containing available services
+   */
   function createFileService(baseUri){
     var endpoints = {
       list: "/api/DocumentStore/List",
       getToken: "/api/DocumentStore/Authorize"
     };
+
+    /**
+     * This is the signature for a standard node style error-first callback.
+     * @callback Node~errorFirstCallback
+     * @param {(null | Error)} err null if there was no error. JS Error if there was and error
+     * @param {*} response The data to be returned if successful
+     */
+
+    /**
+     * Private convenience service for use in building URI's
+     * @param {string}endpointName key to use in looking up the relative path for the endpoint --
+     * Key should be a property of the private createFileService.endpoints.
+     * @returns {string} the full Uri for the desired WAM API endpoint
+     */
     function buildEndpointUrl(endpointName){
       var endpoint = endpoints[endpointName];
       if(!endpoint){
@@ -29,43 +56,62 @@
       return baseUri + endpoint;
     }
 
+    /**
+     * Get a listing of all the files in a container.
+     * @param {string} container The container in which the files are located
+     * @param {Node~errorFirstCallback} cb - Returns an array of [{{name, link}}]
+     */
     function listFilesInContainer(container, cb){
       $.ajax({
         method: "post",
-        dataType: 'json',
-        contentType: 'application/json',
+        dataType: "json",
+        contentType: "application/json",
         url: buildEndpointUrl("list"),
         data: JSON.stringify({
-          container: container,
+          container: container
         }),
         success: function (response) {
           cb(null, response.links);
         },
         error: function( jqXHR, textStatus, errorThrown){
-          cb(new Error(jqXHR.responseText));
+          cb(new Error(jqXHR.responseText), null);
         }
       });
     }
+
+    /**
+     * Get a link that will allow the browser to download the desired file from Azure
+     * @param {string} name The name of the file that is desired
+     * @param {string} link The WAM API link that will return a limited use, tokenized download link
+     * @param {Node~errorFirstCallback} cb returns a single {{tokenizedLink}}
+     */
     function getTokenizedFileLink(name, link, cb) {
       $.ajax({
         method: "get",
-        dataType: 'json',
-        contentType: 'application/json',
+        dataType: "json",
+        contentType: "application/json",
         url: link,
         success: function (response) {
           var tokenizedLink = response.uri + response.token;
           cb(null, {tokenizedLink: tokenizedLink});
         },
         error: function (jqXHR, textStatus, errorThrown) {
-          cb(new Error(jqXHR.responseText));
+          cb(new Error(jqXHR.responseText), null);
         }
       });
     }
+
+    /**
+     * Contacts the WAM API to request
+     * @param {string} container the container where the file should be uploaded.
+     * @param {File} file the file input containing the selected file
+     * @param {Node~errorFirstCallback} cb returns the speed summary from the upload operation
+     */
     function uploadFile(container, file, cb) {
       $.ajax({
         method: "post",
-        dataType: 'json',
-        contentType: 'application/json',
+        dataType: "json",
+        contentType: "application/json",
         url: buildEndpointUrl("getToken"),
         data: JSON.stringify({
           container: container,
@@ -76,17 +122,17 @@
           var summary = blobService.createBlockBlobFromBrowserFile(container,
             file.name,
             file,
-            (error, result) => {
+            function(error, result) {
               if(error) {
-                cb(error);
+                cb(error, null);
               } else {
                 cb(null, summary);
-                console.log('Upload is successful');
+                console.log("Upload is successful");
               }
             });
         },
         error: function (jqXHR, textStatus, errorThrown) {
-          cb(new Error(jqXHR.responseText));
+          cb(new Error(jqXHR.responseText), null);
         }
       });
     }
@@ -94,8 +140,12 @@
       listFilesInContainer: listFilesInContainer,
       getTokenizedFileLink: getTokenizedFileLink,
       uploadFile: uploadFile
-    }
+    };
   }
+  var myApp = {};
+  myApp.wamFileService = createFileService("http://localhost:62435");
+  myApp.utils = createUtils();
+  return myApp;
 })(jQuery);
 
 
@@ -106,6 +156,12 @@ $(function (app) {
   // get a response from Maximo
   $("input[name='tempId']").val(app.utils.createUuid());
 
+  /**
+   * Updates a container with a button and label.  When clicked the button will call getFile to download it.
+   * @param {string} id the element that will contain the file list
+   * @param [string} name the label for the file -- most likely the file nanme
+   * @param {string} link the link that will be used to acquire a token for the file
+   */
   function addFileDownloadElement(id, name, link){
     var $container = $("<div>", {style: "margin-bottom: 5px;"});
     var $filename = $("<div>", {style: "display: inline-block;"}).append("<span/>").text(name);
@@ -119,6 +175,9 @@ $(function (app) {
     $("#" + id).append($container);
   }
 
+  /**
+   * Adds the files in a container to the filelist element in the page
+   */
   function listFilesAndDirectories(){
     var tempContainer = $("input[name='tempId']").val();
     fileService.listFilesInContainer(tempContainer, function(err, data){
@@ -134,8 +193,11 @@ $(function (app) {
     });
   }
 
-  /*
-    Go to WAM api and receive a link from which the document may be downloaded
+  /**
+   * Go to WAM api and receive a link from which the document may be downloaded.  Once the link is returned, the link
+   * is clicked and downloaded to the browser (assumes that the Content-Disposition element has been set appropriately
+   * @param {string} name the file name
+   * @param {string} link the link to the WAM Api that will provide a link that may be used in the client browser
    */
   function getFile(name, link){
     fileService.getTokenizedFileLink(name, link, function(err, data){
@@ -143,14 +205,18 @@ $(function (app) {
         throw err;
       }
       var uri = data.tokenizedLink;
-      var link = document.createElement("a");
-      link.download = name;
-      link.href = uri;
-      link.click();
+      var a = document.createElement("a");
+      a.download = name;
+      a.href = uri;
+      a.click();
     });
   }
 
-  // Wire up the upload button
+  /**
+   * When the page is loaded, get the id for the container. In this case, it is a temporary id set up
+   * by the page on load.  This id should be replaced with one that allows for the system to identify the user
+   * accessing the page and authorize access to the files
+   */
   $("#upload").click(function () {
     var fileUploader = $("#fileupload");
     var file = fileUploader[0].files[0];
@@ -158,8 +224,8 @@ $(function (app) {
     fileService.uploadFile(tempContainer, file, function(err, data){
       if(err){
         throw err;
-      };
-      fileUploader.replaceWith(fileUploader.val('').clone(true));
+      }
+      fileUploader.replaceWith(fileUploader.val("").clone(true));
       listFilesAndDirectories();
     });
   });
